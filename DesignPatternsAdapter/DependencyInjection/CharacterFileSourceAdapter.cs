7f058c415742cd649dda38d6fae8c02a86b22779
Edin Jašarević <edinjasar14@gmail.com>
2021-11-22 21:05:26 +0100

    dependency injection

---
+++ DesignPatternsAdapter/DependencyInjection/CharacterFileSourceAdapter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsAdapter.DependencyInjection
+{
+    public class CharacterFileSourceAdapter : ICharacterSourceAdapter
+    {
+        private string _fileName;
+        private readonly CharacterFileSource _characterFileSource;
+
+        public CharacterFileSourceAdapter(string fileName, CharacterFileSource characterFileSource)
+        {
+            _fileName = fileName;
+            _characterFileSource = characterFileSource;
+        }
+
+        public async Task<IEnumerable<Person>> GetCharacters()
+        {
+            return await _characterFileSource.GetCharactersFromFile(_fileName);
+        }
+    }
+}
