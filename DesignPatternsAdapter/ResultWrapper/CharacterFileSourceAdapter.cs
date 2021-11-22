e9e10e27cb3dccd6f42dd315f5cbaabefe3a319f
Edin Jašarević <edinjasar14@gmail.com>
2021-11-22 21:13:58 +0100

    result wrapper

---
+++ DesignPatternsAdapter/ResultWrapper/CharacterFileSourceAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsAdapter.ResultWrapper
+{
+    public class CharacterFileSourceAdapter : ICharacterSourceAdapter
+    {
+        private string _fileName;
+        private readonly CharacterFileSource _characterFileSource;
+
+        public CharacterFileSourceAdapter(string fileName, CharacterFileSource characterFileSource)
+        {
+            _fileName = fileName;
+            _characterFileSource = characterFileSource;
+        }
+
+        public async Task<IEnumerable<Person>> GetCharacters() => (await _characterFileSource
+                .GetCharactersFromFile(_fileName))
+            .Select(character => new CharacterToPersonAdapter(character));
+    }
+}
---
+++ DesignPatternsAdapterTest/ResultWrapperTest.cs
@@ -0,0 +1,40 @@
+﻿using DesignPatternsAdapter.DependencyInjection;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DesignPatternsAdapterTest
+{
+    public class ResultWrapperTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public ResultWrapperTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public async Task DisplayCharactersFromFile()
+        {
+            string filename = @"People.json";
+            var service = new StarWarsCharacterDisplayService(
+                new CharacterFileSourceAdapter(filename, new CharacterFileSource()));
+
+            var result = await service.ListCharacters();
+
+            _output.WriteLine(result);
+        }
+
+        [Fact]
+        public async Task DisplayCharactersFromApi()
+        {
+            var service = new StarWarsCharacterDisplayService(
+                new StarWarsApiCharacterSourceAdapter(new StarWarsApi()));
+
+            var result = await service.ListCharacters();
+
+            _output.WriteLine(result);
+        }
+    }
+}
