5686cf0a8f39944d7a39e1ba45e8d85c9198f5d0
Edin Jašarević <edinjasar14@gmail.com>
2021-11-22 19:17:47 +0100

    init

---
+++ DesignPatternsAdapter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatternsAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
