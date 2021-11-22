dc702483817b8890f283d820d600bd7106f46a19
Edin Jašarević <edinjasar14@gmail.com>
2021-11-22 19:44:23 +0100

    two provider classes

---
+++ DesignPatternsAdapter/TwoProvidersClasses/StarWarsCharacterDisplayService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsAdapter.TwoProvidersClasses
+{
+    public class StarWarsCharacterDisplayService
+    {
+        public enum CharacterSource
+        {
+            File,
+            Api
+        }
+        public async Task<string> ListCharacters(CharacterSource source)
+        {
+            List<Person> people;
+            if (source == CharacterSource.File)
+            {
+                string filePath = @"People.json";
+                var characterSource = new CharacterFileSource();
+                people = await characterSource.GetCharactersFromFile(filePath);
+            }
+            else if (source == CharacterSource.Api)
+            {
+                var swapiSource = new StarWarsApi();
+                people = await swapiSource.GetCharacters();
+            }
+            else
+            {
+                throw new Exception("Invalid character source");
+            }
+            var sb = new StringBuilder();
+            int nameWidth = 30;
+            sb.AppendLine($"{"NAME".PadRight(nameWidth)}   {"HAIR"}");
+            foreach (Person person in people)
+            {
+                sb.AppendLine($"{person.Name.PadRight(nameWidth)}   {person.HairColor}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ DesignPatternsAdapterTest/TwoProvidersClassesTest.cs
@@ -0,0 +1,37 @@
+﻿using DesignPatternsAdapter.TwoProvidersClasses;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DesignPatternsAdapterTest
+{
+    public class TwoProvidersClassesTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public TwoProvidersClassesTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public async Task DisplayCharactersFromFile()
+        {
+            var service = new StarWarsCharacterDisplayService();
+
+            var result = await service.ListCharacters(StarWarsCharacterDisplayService.CharacterSource.File);
+
+            _output.WriteLine(result);
+        }
+
+        [Fact]
+        public async Task DisplayCharactersFromApi()
+        {
+            var service = new StarWarsCharacterDisplayService();
+
+            var result = await service.ListCharacters(StarWarsCharacterDisplayService.CharacterSource.Api);
+
+            _output.WriteLine(result);
+        }
+    }
+}
